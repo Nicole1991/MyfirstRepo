a5da0815e64e9f6839691264146b1f65e3c848a2
Nicole <qyzhao1991@gmail.com>
2017-02-21 15:17:05 +0800

    git commit to branch test1

---
+++ src/triangleexercises/EasiestExcercies.java
@@ -0,0 +1,11 @@
+package triangleexercises;
+
+/**
+ * Created by zhaoqiaoying on 2017/2/20.
+ */
+public class EasiestExcercies {
+    public static void main(String[] args) {
+        System.out.print("*");
+        System.out.println();
+    }
+}
