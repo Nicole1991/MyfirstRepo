fa55fa31dd26f1d3c6b2a89c8f94772966b6e8aa
Nicole1991 <qyzhao1991@gmail.com>
2017-02-21 10:40:13 +0800

    Add files via upload

---
+++ EasiestExcercies.java
@@ -0,0 +1,10 @@
+package triangleexercises;
+
+/**
+ * Created by zhaoqiaoying on 2017/2/20.
+ */
+public class EasiestExcercies {
+    public static void main(String[] args) {
+        System.out.print("*");
+    }
+}
